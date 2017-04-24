4412249a1c8eaee2916aec31b1ab6e27d3032d73
Vineesh_Devasia <vinihi83@gmail.com>
2017-04-24 11:41:47 +0530

    Initial commit from spring starter project (config server,actuator).

---
+++ outsight-config-server/src/main/java/com/outsight/config/OutsightConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.outsight.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsightConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutsightConfigServerApplication.class, args);
+	}
+}
