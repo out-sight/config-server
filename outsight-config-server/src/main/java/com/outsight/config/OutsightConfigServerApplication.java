cabd50af8d71b2749222fc6bc7b0626ae4808332
Vineesh_Devasia <vinihi83@gmail.com>
2017-04-24 13:32:15 +0530

    configured git uri (pinting to local FS as github urls were giving proxy issues).
    disabled the actuator security for now.

--- outsight-config-server/src/main/java/com/outsight/config/OutsightConfigServerApplication.java
+++ outsight-config-server/src/main/java/com/outsight/config/OutsightConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.outsight.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class OutsightConfigServerApplication {
 
 	public static void main(String[] args) {
